c36f3465abb04bdf57303896cf921024f8a01b1e
CrownClown00 <rossca.thompson@gmail.com>
2015-07-01 14:25:54 -0700

    Added cool pose animation as well as instruction text

--- js/app.js
+++ js/app.js
@@ -9,14 +9,46 @@ $(document).ready(function() {
   })
     
     .mousedown(function() {
-    // play hadouken sound
+    playHadouken();// play hadouken sound
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // animate hadouken to the right of the screen
+    $('.hadouken').finish().show().animate(
+    {'left': '1020px'},
+        500,
+    function() {
+    $(this).hide();
+    $(this).css('left', '530px');
+  }
+    );
   })
   .mouseup(function() {
     $('.ryu-throwing').hide();
   $('.ryu-ready').show();
   });
+
+  $(document).keydown(function( event){
+     
+  if ( event.which == 88 ) {
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+      console.log("hi");
+  }
+      
+    
+      
+
+ }).keyup(function(event){
+      if(event.which==88){
+          $('.ryu-cool').hide();
+          $('.ryu-ready').show();
+      }
+      
+  });
 });
+
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
